1dc18314ac9ef3b318583889ea6e9e49eb4a3028
thinkfull <378508297@qq.com>
2016-09-28 22:53:03 +0800

    committed by thinfull2 from his local workspaces, second time commit

--- socket/src/create/socket/JabberClient.java
+++ socket/src/create/socket/JabberClient.java
@@ -21,6 +21,8 @@ import java.net.UnknownHostException;
 * 3.  在本地作的修改，同时在github也做修改，看看冲突(这一行是本地修改的)
 
 * 5.  这一行是由thinkfull2增加的，并由thinkfull提交到自己的branch上
+
+* 6.  这一行也是由thinkfull2增加的，并由thinkfull提交到自己的branch上,第二次提交
 **/
 public class JabberClient {
 
